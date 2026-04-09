e822b92a7e8ddd65a6836fee288cf8679e62d1c3
SkySoul9 <alextoto2005@gmail.com>
2026-04-09 23:41:41 +0200

    Add files via upload
    Signed-off-by: SkySoul9 <alextoto2005@gmail.com>

--- src/Interface_Messagerie.java
+++ src/Interface_Messagerie.java
@@ -1,113 +1,50 @@
 import base_donnee.Connexion;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
+import javafx.scene.layout.Border;
+
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 
 public class Interface_Messagerie extends JFrame{
-    final static int Largeur = 600;
+    final static int Largeur = 1000;
 	final static int Hauteur = 600;
-    private String user;
-    //protected  String mdp;
-    private String m = null;
     JPanel chatPanel;
-    Connexion c;
     int i = 1;
-    ClientMajuscule cl;
+  
+    
+    Connexion c;
 
     public Interface_Messagerie(String username, ClientMajuscule cl) throws SQLException{
-        try {
-            c = new Connexion();
-            System.out.println("Connecte a la bd");
-        } catch (Exception e) {
-        }
+        c = new Connexion();
+        setTitle("Messagerie");
+        setSize(Largeur, Hauteur);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+        // Onglets
+        JTabbedPane tabbedPane = new JTabbedPane();
+
+        JPanel chatPanel = new JPanel(new BorderLayout());
+
+        // Partie gauche : messages
+        JPanel JPGauche = new JPanel(new BorderLayout());
+        JTextArea zoneMSG = new JTextArea();
+        zoneMSG.setEditable(false);
+
+        JScrollPane chatScroll = new JScrollPane(zoneMSG);
         
-        
-        this.user = username;
-        this.cl = cl;
-        //BorderLayout b1 = new BorderLayout();
-        JPanel p1 = new JPanel(new BorderLayout(20,20)); // panel interface complète
-        chatPanel = new JPanel(); // panel interface des messages
-        BoxLayout box_chat = new BoxLayout(chatPanel, BoxLayout.PAGE_AXIS);
-        chatPanel.setLayout(box_chat);
-        //chatPanel.setBackground(Color.gray); // changement de couleur
-        JPanel p3 = new JPanel(new GridLayout(1, 3, 50, 50));
-        //p3
-        JMenuBar menu = new JMenuBar();
-
-        // bouton pour ajouter des groupes et utilisateurs
-        //JButton add_user = new JButton("Ajouter Utilisateur");
-        JLabel statut = new JLabel();
-        
-        
-        JButton add_groupe = new JButton("Ajout Groupe");
-        
-        add_groupe.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e){
-                
-                JDialog addG = new JDialog(getOwner());
-                JTextField t = new JTextField("entrez le nom du groupe");
-                addG.add(t);
-                this.add(addG);
-                if(t.getText() != null){
-                    addGroupe(i, t.getText());
-                    JMenuItem g =  new JMenuItem(t.getText());
-                    add_groupe.add(g);
-                    i += 1;
-            }
-            }
-
-            private void add(JDialog addG) {
-                throw new UnsupportedOperationException("Not supported yet.");
-            }
-        });
-
-        // Size pour les boutons d'ajout
-        add_groupe.setSize(30, 30);
-        //add_user.setSize(30, 30);
-        // elements du menu
-        JMenu groupe = new JMenu("Groupes");
-        JMenu utilisateur = new JMenu("Utilisateurs");
-        JMenu setting = new JMenu("Paramètre");
-
-        // Items des menus
-        JMenuItem log_out = new JMenuItem("Déconnexion"); 
-        log_out.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e){
-                //user = null;
-                System.exit(0);
-                System.out.println("Déconnexion réussi");
-                try {
-                    c.query_maj("UPDATE utilisateur SET statut = 0");
-                    statut.setText(user + " : déconnecté");
-                } catch (SQLException e1) {
-                    // TODO Auto-generated catch block
-                    e1.printStackTrace();
-                }
-
-            }
-        });
-        setting.add(log_out);
-
-        // ajout des éléments
-        menu.add(groupe);
-        menu.add(utilisateur);
-        menu.add(setting);
-        menu.add(statut);
-        menu.add(add_groupe);
-
-
-        JLabel message = new JLabel("message :");
         JTextField text = new JTextField();
-        JButton envoyer = new JButton("envoyer");
+        JButton envoyer = new JButton("Envoyer");
         envoyer.addActionListener(new ActionListener() { // methode qui envoie un message et créé des bulles
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -123,84 +60,202 @@ public class Interface_Messagerie extends JFrame{
                     }
                     text.setText("");
                 }
-                
-                //throw new UnsupportedOperationException("Not supported yet.");
             }
         
         });
         
-         
-        p3.add(message);
-        p3.add(text);
-        p3.add(envoyer);
+        JPanel envoieP = new JPanel(new BorderLayout());
+        envoieP.add(text, BorderLayout.CENTER);
+        envoieP.add(envoyer, BorderLayout.EAST);
+
+        JPGauche.add(chatScroll, BorderLayout.CENTER);
+        JPGauche.add(envoieP, BorderLayout.SOUTH);
+
+
+        // Partie a droite
+        JPanel JPDroit = new JPanel(new BorderLayout());
+        CardLayout MenuCartes = new CardLayout();
+        JPanel JPSelect = new JPanel(MenuCartes);
         
 
-        // interface message (p2)
+        //Menu de séléction conversation Utilisateur ou groupe grâce a des boutons 
+        JButton JButilisateurs = new JButton("Utilisateurs");
+        JButton JBgroupes = new JButton("Groupes");
         
-        p1.add(menu, BorderLayout.PAGE_START );
-        p1.add(chatPanel, BorderLayout.CENTER);
-        p1.add(p3, BorderLayout.PAGE_END);
-        this.add(p1);
-        
-
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle("Messagerie");
-		setResizable(false);
-		setSize(Largeur,Hauteur);
-		setVisible(true);
+        JPanel JLMenu = new JPanel();
+        JLMenu.add(JButilisateurs);
+        JLMenu.add(JBgroupes);
 
         
-    }
-    
-public void listen_reception_mess(){
-    new Thread(() -> {
-        try {
-            while (true) {
-                // On lit dans le thread secondaire (NE BLOQUE PAS L'UI)
-                final String m = this.cl.readMessage(); 
+
+
+        //Partie conv utilisateur
+        JPanel JPUtilisateur = new JPanel(new BorderLayout()); //création d'un panel pour stocké la liste des utilisateurs
+
+        String rqListUser = "SELECT nom FROM utilisateur;";
+        c.query_select(rqListUser);
+        
+        List<String> ListUser = new ArrayList<>();
+
+        while(c.rs.next()){ //Rentre les utilisateurs de la rqt SQL pour les afficher
+            ListUser.add(c.rs.getString("nom"));
+        }
+
+        JList<String> JListUser = new JList<>(ListUser.toArray(new String[0]));
+        JScrollPane JSPUser = new JScrollPane(JListUser);
+
+
+        JPUtilisateur.add(JSPUser,BorderLayout.CENTER);
+        JPSelect.add(JPUtilisateur,"Utilisateurs");
+        JButilisateurs.addActionListener(new ActionListener() { 
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MenuCartes.show(JPSelect,"Utilisateurs");
+            }
+        });
+
+
+
+        //Partie conv groupes
+        JPanel JPgroupes = new JPanel(new BorderLayout());
+        //rqt qui prend les grps auquel l'utilisateur de l'interface appartient
+        String rqListGrp = "SELECT nom_grp FROM groupe g JOIN utilisateur_groupe ug ON g.id_grp = ug.id_grp WHERE id_user = (SELECT id_user FROM utilisateur WHERE nom = '" + username + "');";
+        c.query_select(rqListGrp);
+        List<String> ListGrp = new ArrayList<>();
+        while(c.rs.next()){
+            ListGrp.add(c.rs.getString("nom_grp"));
+        }
+        JList<String> JListGrp = new JList<>(ListGrp.toArray(new String[0]));
+        JScrollPane JSPGrp = new JScrollPane(JListGrp);
+        JPgroupes.add(JSPGrp,BorderLayout.CENTER);
+        JPSelect.add(JPgroupes, "Groupes");
+        JBgroupes.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                MenuCartes.show(JPSelect,"Groupes");
+            }
+        });
+
+        JPDroit.add(JLMenu,BorderLayout.NORTH);
+        JPDroit.add(JPSelect,BorderLayout.CENTER);
+        chatPanel.add(JPDroit);
+
+        //Séparation des 2 panel pour avoir 2 colonnes
+        JSplitPane separation = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, JPGauche, JPDroit);
+        separation.setResizeWeight( 0.9 );
+
+        chatPanel.add(separation, BorderLayout.CENTER);
+
+
+
+        //Barre d'information utilisateur 
+        JPanel infoPanel = new JPanel(new GridLayout(1, 3));
+
+        JLabel LabelStatut = new JLabel("Vous êtes connecté");
+        infoPanel.add(LabelStatut);
+
+        JLabel DiscussActuel = new JLabel("Discussion avec Personne");
+        infoPanel.add(DiscussActuel);
+
+        JLabel LabelNomUser = new JLabel("Vous êtes : "+ username);
+        infoPanel.add(LabelNomUser);
+
+
+
+        JListUser.addListSelectionListener(new ListSelectionListener() {
+            public void valueChanged(ListSelectionEvent e) {
+                DiscussActuel.setText(JListUser.getSelectedValue());
+                zoneMSG.setText("");
+            }
+        });
+
+        JListGrp.addListSelectionListener(new ListSelectionListener() {
+            public void valueChanged(ListSelectionEvent e) {
+                DiscussActuel.setText(JListGrp.getSelectedValue());
+                zoneMSG.setText("");
+            }
+        });
+
+
+
+
+        //Menu parametre
+        JMenuBar setting = new JMenuBar();
+        JMenu parametre = new JMenu("Paramètre");
+        JMenuItem log_out = new JMenuItem("Déconnexion"); 
+        log_out.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
                 
-                if (m != null) {
-                    // On met à jour l'UI sur le thread graphique
-                    SwingUtilities.invokeLater(() -> {
-                        addMessage(m, false);
-                    });
+                try {
+                    c.query_select("SELECT statut FROM utilisateur WHERE nom ='" + username +"';");
+                    if(c.rs.next() && c.rs.getInt("statut")== 1){
+                        c.query_maj("UPDATE utilisateur SET statut = 0 WHERE nom = '"+ username +"';");
+                        log_out.setText("Connection");
+                        LabelStatut.setText("Vous êtes deconnecté");
+                        DiscussActuel.setText("Discussion avec Personne");
+                        LabelNomUser.setText("Vous êtes personne");
+                        zoneMSG.setText("");
+                    }else {
+                        new Interface_Connexion();
+                        setVisible(false);
+
+                    }
+
+                } catch (SQLException e1) {
+                    
+                    e1.printStackTrace();
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }).start();
-}
-
-// 2. Méthode pour ajouter une bulle
-public void addMessage(String text, boolean isMe) {
-    // Panel de ligne pour l'alignement (Gauche ou Droite)
-    JPanel line = new JPanel(new FlowLayout(isMe ? FlowLayout.RIGHT : FlowLayout.LEFT));
-    line.setMaximumSize(new Dimension(Integer.MAX_VALUE, 50)); // Empêche de prendre trop de hauteur
-    
-    // La bulle de message
-    JLabel bubble = new JLabel(text);
-    bubble.setOpaque(true);
-    bubble.setBackground(isMe ? Color.CYAN : Color.RED);
-    bubble.setBorder(BorderFactory.createEmptyBorder(10, 15, 10, 15)); // Le "padding" interne
-    
-    line.add(bubble);
-    chatPanel.add(line);
-    
-    // Espace entre les messages (le fameux gap)
-    chatPanel.add(Box.createRigidArea(new Dimension(0, 10))); 
-    
-    chatPanel.revalidate();
-}
-
-    public void addGroupe(int id, String n){
-        Groupe g = new Groupe(id, n, 10);
-    }
-    public static void main(String[] args) throws SQLException, IOException {
-        //ClientMajuscule cl = new ClientMajuscule("midorima");
-        //ClientMajuscule cl2 = new ClientMajuscule("kagami");
-        //new Interface_Messagerie(cl.getNom(),cl).listen_reception_mess();;
-        //new Interface_Messagerie(cl2.getNom(),cl2).listen_reception_mess();
+        });
+        parametre.add(log_out);
         
+
+        JPanel JPanelNord = new JPanel(new FlowLayout());
+
+        JMenuItem Decoration = new JMenuItem("Décoration");
+        Decoration.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                int r=Personnalisation.color();
+                int b=Personnalisation.color();
+                int g=Personnalisation.color();
+                JPanelNord.setBackground(new Color(r,g,b));
+                envoieP.setBackground(new Color(r,g,b));
+                JLMenu.setBackground(new Color(r,g,b));
+            }
+        });
+        parametre.add(Decoration);
+        setting.add(parametre);
+
+        
+        JPanelNord.add(setting);
+        JPanelNord.add(infoPanel);
+        add(JPanelNord,BorderLayout.NORTH);
+        add(chatPanel,BorderLayout.CENTER);
     }
+
+    public void addMessage(String text, boolean isMe) {
+            // Panel de ligne pour l'alignement (Gauche ou Droite)
+            JPanel line = new JPanel(new FlowLayout(isMe ? FlowLayout.RIGHT : FlowLayout.LEFT));
+            line.setMaximumSize(new Dimension(Integer.MAX_VALUE, 50)); // Empêche de prendre trop de hauteur
+    
+            // La bulle de message
+            JLabel bubble = new JLabel(text);
+            bubble.setOpaque(true);
+            bubble.setBackground(isMe ? Color.CYAN : Color.RED);
+            bubble.setBorder(BorderFactory.createEmptyBorder(10, 15, 10, 15)); // Le "padding" interne
+    
+            line.add(bubble);
+            chatPanel.add(line);
+    
+            // Espace entre les messages (le fameux gap)
+            chatPanel.add(Box.createRigidArea(new Dimension(0, 10))); 
+    
+            chatPanel.revalidate();
+        }
+
+
+    public static void main(String[] args) {
+    }
+
+
 }
